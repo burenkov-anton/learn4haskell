f3915d69f1db2cf9e958e64948bebdd6d1bdc545
Anton Burenkov <buran665@yandex.ru>
2020-11-04 19:44:45 +0700

    solution for chapter 4

--- src/Chapter4.hs
+++ src/Chapter4.hs
@@ -114,23 +114,23 @@ As always, try to guess the output first! And don't forget to insert
 the output in here:
 
 >>> :k Char
-
+Char :: *
 >>> :k Bool
-
+Bool :: *
 >>> :k [Int]
-
+[Int] :: *
 >>> :k []
-
+[] :: * -> *
 >>> :k (->)
-
+(->) :: * -> * -> *
 >>> :k Either
-
+Either :: * -> * -> *
 >>> data Trinity a b c = MkTrinity a b c
 >>> :k Trinity
-
+Trinity :: * -> * -> * -> *
 >>> data IntBox f = MkIntBox (f Int)
 >>> :k IntBox
-
+IntBox :: (* -> *) -> *
 -}
 
 {- |
@@ -293,7 +293,8 @@ values and apply them to the type level?
 -}
 instance Functor (Secret e) where
     fmap :: (a -> b) -> Secret e a -> Secret e b
-    fmap = error "fmap for Box: not implemented!"
+    fmap f (Trap c) = Trap c
+    fmap f (Reward d) = Reward (f d)
 
 {- |
 =⚔️= Task 3
@@ -305,7 +306,12 @@ typeclasses for standard data types.
 -}
 data List a
     = Empty
-    | Cons a (List a)
+    | Cons a (List a) deriving (Show)
+
+instance Functor List where
+    fmap :: (a -> b) -> List a -> List b
+    fmap f Empty = Empty
+    fmap f (Cons a xs) = Cons (f a) (fmap f xs)
 
 {- |
 =🛡= Applicative
@@ -472,10 +478,13 @@ Implement the Applicative instance for our 'Secret' data type from before.
 -}
 instance Applicative (Secret e) where
     pure :: a -> Secret e a
-    pure = error "pure Secret: Not implemented!"
+    pure x = Reward x
 
     (<*>) :: Secret e (a -> b) -> Secret e a -> Secret e b
-    (<*>) = error "(<*>) Secret: Not implemented!"
+    (<*>) (Trap f) x = Trap f
+    (<*>) f (Trap x) = Trap x
+    (<*>) (Reward f) (Reward x) = Reward (f x)
+    --(<*>) = error "(<*>) Secret: Not implemented!"
 
 {- |
 =⚔️= Task 5
@@ -488,6 +497,20 @@ Implement the 'Applicative' instance for our 'List' type.
   may also need to implement a few useful helper functions for our List
   type.
 -}
+
+append :: List a -> List a -> List a
+append Empty x = x
+append x Empty = x
+append (Cons x xs) ys = Cons x (append xs ys)
+
+instance Applicative List where
+    pure :: a -> List a
+    pure x = Cons x Empty
+
+    (<*>) :: List (a -> b) -> List a -> List b
+    (<*>) Empty x = Empty
+    (<*>) x Empty = Empty
+    (<*>) (Cons f fx) (Cons x xs) = Cons (f x) (append (fmap f xs) (fx <*> (Cons x xs)))
 
 
 {- |
@@ -600,7 +623,8 @@ Implement the 'Monad' instance for our 'Secret' type.
 -}
 instance Monad (Secret e) where
     (>>=) :: Secret e a -> (a -> Secret e b) -> Secret e b
-    (>>=) = error "bind Secret: Not implemented!"
+    (>>=) (Trap x) f = Trap x
+    (>>=) (Reward x) f =  f x
 
 {- |
 =⚔️= Task 7
@@ -610,6 +634,11 @@ Implement the 'Monad' instance for our lists.
 🕯 HINT: You probably will need to implement a helper function (or
   maybe a few) to flatten lists of lists to a single list.
 -}
+
+instance Monad List where
+    (>>=) :: List a -> (a -> List b) -> List b
+    (>>=) Empty _ = Empty
+    (>>=) (Cons x xs) f = append (f x) ((>>=) xs f)
 
 
 {- |
@@ -629,7 +658,9 @@ Can you implement a monad version of AND, polymorphic over any monad?
 🕯 HINT: Use "(>>=)", "pure" and anonymous function
 -}
 andM :: (Monad m) => m Bool -> m Bool -> m Bool
-andM = error "andM: Not implemented!"
+andM x y = x >>= \a -> if a then y else pure False
+--andM x y = pure (&&) <*> x <*> y
+
 
 {- |
 =🐉= Task 9*: Final Dungeon Boss
@@ -673,6 +704,20 @@ Specifically,
  ❃ Implement the function to convert Tree to list
 -}
 
+data Tree x = EmptyTree | Node (Tree x) x (Tree x) deriving (Show)
+
+instance Functor Tree where
+    fmap :: (a -> b) -> Tree a -> Tree b
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node (left) x (right)) = Node (fmap f left) (f x) (fmap f right)
+
+reverseTree :: Tree a -> Tree a
+reverseTree EmptyTree = EmptyTree
+reverseTree (Node (left) x (right)) = Node (reverseTree right) x (reverseTree left)
+
+treeToList :: Tree a -> [a]
+treeToList EmptyTree = []
+treeToList (Node left x right) = [x] ++ treeToList left ++ treeToList right
 
 {-
 You did it! Now it is time to open pull request with your changes
